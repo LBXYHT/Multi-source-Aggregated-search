ad0e1379cb4eecd18f8eeeb82b0cb940d8f40b1f
viego <viego@nyu.com>
2025-07-16 17:36:20 -0400

    backend modified

--- hutao-backend/src/main/java/com/hutao/aggregatedsearch/model/vo/SearchVO.java
+++ hutao-backend/src/main/java/com/hutao/aggregatedsearch/model/vo/SearchVO.java
@@ -9,8 +9,6 @@ import java.util.List;
 /**
  * 聚合搜索
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @Data
 public class SearchVO implements Serializable {
